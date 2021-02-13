0a21bd9f7f496450698a0f3cb4699b006dbb7215
BisratAsfaw22 <bisratlingereh@gmail.com>
2021-02-12 22:03:37 -0500

    code update

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,18 @@
 /* Put your javascript in here */
+
+const carsoulArray = [];
+for (let i = 0; i < document.querySelectorAll('.carousel_img').length; i++) {
+  slideArray.push(document.querySelectorAll('.carousel_img')[i].dataset.background);
+}
+
+console.log(carsouleArray);
+let currentSlideIndex = -1;
+
+
+function advanceSliderItem() {
+  currentSlideIndex++;
+
+  if (currentSlideIndex >= carouselArray.length) {
+    currentSlideIndex = 0;}
+
+  }
