1e371bc21f0b59e34833531b2ea98d266b18b88a
BisratAsfaw22 <bisratlingereh@gmail.com>
2021-02-13 18:39:59 -0500

    image fix

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,18 +1,70 @@
 /* Put your javascript in here */
 
-const carsoulArray = [];
-for (let i = 0; i < document.querySelectorAll('.carousel_img').length; i++) {
-  slideArray.push(document.querySelectorAll('.carousel_img')[i].dataset.background);
-}
+let slideposition = 0;
+const slides = document.getElementsByClassName('images');
+const totalSlides = slides.length;
 
-console.log(carsouleArray);
-let currentSlideIndex = -1;
+document.
+  getElementsByClassName('arrow next')
+  .addEventListener("click", function() {
+    moveToNextSlide();
+  });
+  document.
+  getElementsByClassName('arrow prev')
+  .addEventListener("click", function() {
+    moveToPrevSlide();
+  });
 
-
-function advanceSliderItem() {
-  currentSlideIndex++;
-
-  if (currentSlideIndex >= carouselArray.length) {
-    currentSlideIndex = 0;}
-
+  function updateSlidePosition() {
+    for (let slide of slides){
+      slide.classList.remove('images--visible');
+      slide.classList.remove('images--hidden');
+    }
+    slides[slidePosition].classList.add('images--visible');
   }
+
+
+  function moveToNextSlide() {
+    if (slideposition == totalSlides-1){
+      slideposition = 0;
+    } else {
+      slidePosition++;
+    }
+    updateSlidePosition();
+  }
+
+  function moveToPrevSlide() {
+    if (slideposition == 0){
+      slideposition = totalSlides - 1;
+    } else {
+      slidePosition--;
+    }
+    updateSlidePosition();
+  }
+
+
+
+
+
+
+
+// const carouselArray = [];
+// for (let i = 0; i < carouselArray.querySelectorAll('.carsoul_img div').length; i++) {
+// carousellArray.push(carouselArray.querySelectorAll('.carsoul_img div')[i].src);
+// }
+
+// console.log(carouselArray);
+// let currentSlideIndex = -1;
+
+
+// function advanceSliderItem() {
+// currentSlideIndex++;
+
+// if (currentSlideIndex >= carouselArray.length) {
+// currentSlideIndex = 0;
+// }
+// }
+
+
+// window.advanceslider = advanceSliderItem;
+  
